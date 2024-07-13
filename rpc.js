445fa0330e38eb67abe263df8961755fb81e82ee
ZLKCyber <88219725+Zlkcyber@users.noreply.github.com>
2024-07-13 23:29:11 +0800

    update rpc

--- rpc.js
+++ rpc.js
@@ -1,6 +1,6 @@
 export const mainnetConfig = {
   networkId: "mainnet",
-  nodeUrl: 'https://rpc.mainnet.near.org',  // this is rpc for near mainnet
+  nodeUrl: 'https://near.lava.build',  
   walletUrl: "https://wallet.mainnet.near.org",
   helperUrl: "https://helper.mainnet.near.org",
 };
